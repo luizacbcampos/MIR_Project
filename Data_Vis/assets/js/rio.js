b58fda62c51f5f643969413d49117aba869c7316
Bruno Monteiro <malettabruno@gmail.com>
2021-08-14 19:47:14 -0300

    Agora vai?

--- Data_Vis/assets/js/rio.js
+++ Data_Vis/assets/js/rio.js
@@ -4,7 +4,7 @@ const margin = {top: 20, right: 30, bottom: 0, left: 10},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
+const svg = d3.select("#Rio")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
