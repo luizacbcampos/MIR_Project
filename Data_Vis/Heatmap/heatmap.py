a459c04b47f5c3ea23f9c0b36266a3b8464b2de2
isadorasalles <isadorasalles@dcc.ufmg.br>
2021-08-15 12:10:27 -0300

    novo dataset

--- Data_Vis/Heatmap/heatmap.py
+++ Data_Vis/Heatmap/heatmap.py
@@ -1,23 +1,15 @@
 import pandas as pd
 import os
+import numpy as np
 
 df = pd.read_csv("./MIR/dataframe.csv", dtype={"falsetto":int})
 df = df[["year", "falsetto"]]
 df["count"] = 0
-df = df.groupby(by=["year", "falsetto"], as_index=False).count()
 
+df = df.groupby(["year", "falsetto"]).count()
+
+idx = pd.MultiIndex.from_product([df.index.levels[0], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+df = df.reindex(idx, fill_value=0)
 print(df)
 
-# for i in range(11):
-# 	col = str(i)
-# 	df[col] = 0
-
-# for index, row in df.iterrows():
-# 	fal = str(row["falsetto"])
-# 	df.at[index, fal] = row["count"]
-# print(df)
-
-# df = df.groupby('year', as_index=False).sum()
-# df = df[["year", "0","1", "2", "3","4", "5", "6", "7", "8", "9", "10"]]
-# print(df)
-df.to_csv("falsetto_by_year.csv", index=False)
+df.to_csv("falsetto_by_year.csv")
