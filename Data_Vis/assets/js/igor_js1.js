81a611ef0a728f131240b3152e558bb5310e2155
Luiza Campos <luizacbcampos@gmail.com>
2021-08-16 15:59:23 -0300

    centros

--- Data_Vis/assets/js/igor_js1.js
+++ Data_Vis/assets/js/igor_js1.js
@@ -1,149 +1,149 @@
+// Density
+
+// set the dimensions and margins of the graph
+const margin = {top: 100, right: 170, bottom: 70, left:170},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#density")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          `translate(${margin.left}, ${margin.top})`);
+
+// Get the different categories and count them
+const categories = [
+    //"weeks_charted_all_time",
+    "falsetto",
+    "spoken",
+    "acousticness",
+    "danceability",
+    //"tempo",
+    "energy",
+    "instrumentalness",
+    "liveness",
+    "loudness",
+    "speechiness",
+    "valence"    
+]
+
+function updategraph(year1, year2) {
+  //read data
+  d3.csv("https://raw.githubusercontent.com/luizacbcampos/MIR_Project/main/Data_Vis/assets/data/normalized_ridgeline_map.csv").then(function(data) {
+
+    const n = categories.length
+    //const year1 = 1990
+    //const year2 = 2015
   
+    // Add X axis
+    const x = d3.scaleLinear()
+      .domain([-5, 5])
+      .range([ 0, width ]);
 
-  // set the dimensions and margins of the graph
-  const margin = {top: 200, right: 30, bottom: 70, left:170},
-      width = 800 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
+    svg.append("g")
+      .style("font-size", 13)
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
   
-  // append the svg object to the body of the page
-  const svg = d3.select("#density")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            `translate(${margin.left}, ${margin.top})`);
+    // Create a Y scale for densities
+    const y = d3.scaleLinear()
+      .domain([0, 7]) //////////////////////////////////////////////////////////// TODO
+      .range([ height, 0]);
   
-  function updategraph(year1, year2) {
-    //read data
-    d3.csv("https://raw.githubusercontent.com/luizacbcampos/MIR_Project/main/Data_Vis/assets/data/normalized_ridgeline_map.csv").then(function(data) {
-    
-      // Get the different categories and count them
-      const categories = [
-          //"weeks_charted_all_time",
-          "falsetto",
-          "spoken",
-          "acousticness",
-          "danceability",
-          //"tempo",
-          "energy",
-          "instrumentalness",
-          "liveness",
-          "loudness",
-          "speechiness",
-          "valence"    
-      ]
+    // Create the Y axis for names
+    const yName = d3.scaleBand()
+      .domain(categories)
+      .range([0, height]) //////////////////////////////////////////////////////////// TODO
+      .paddingInner(1)
 
-      const n = categories.length
-      //const year1 = 1990
-      //const year2 = 2015
-    
-      // Add X axis
-      const x = d3.scaleLinear()
-        .domain([-5, 5])
-        .range([ 0, width ]);
+    svg.append("text")
+      .attr("x", (width / 2))             
+      .attr("y", 0 - (3*margin.top / 4))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "30px") 
+      .style("text-decoration", "bold")  
+      .text("Densidade dos atributos por ano selecionado");
+      
+    svg.append("g")
+    .style("font-size", 15)
+      .call(d3.axisLeft(yName));
+  
+    // Compute kernel density estimation for each column:
+    const kde = kernelDensityEstimator(kernelEpanechnikov(1), x.ticks(50)) // increase this 40 for more accurate density.
+    const allDensity1 = []
+    const allDensity2 = []
+    for (i = 0; i < n; i++) {
+        key1 = categories[i] + "_" + year1
+        density_1 = kde( data.map(function(d){  return d[key1]; }) )
 
-      svg.append("g")
-        .style("font-size", 13)
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-    
-      // Create a Y scale for densities
-      const y = d3.scaleLinear()
-        .domain([0, 7]) //////////////////////////////////////////////////////////// TODO
-        .range([ height, 0]);
-    
-      // Create the Y axis for names
-      const yName = d3.scaleBand()
-        .domain(categories)
-        .range([0, height]) //////////////////////////////////////////////////////////// TODO
-        .paddingInner(1)
-
-      svg.append("text")
-        .attr("x", (width / 2))             
-        .attr("y", 0 - (margin.top / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "30px") 
-        .style("text-decoration", "bold")  
-        .text("Densidade dos atributos por ano selecionado");
+        key2 = categories[i] + "_" + year2
+        density_2 = kde( data.map(function(d){  return d[key2]; }) )
         
-      svg.append("g")
-      .style("font-size", 15)
-        .call(d3.axisLeft(yName));
-    
-      // Compute kernel density estimation for each column:
-      const kde = kernelDensityEstimator(kernelEpanechnikov(1), x.ticks(50)) // increase this 40 for more accurate density.
-      const allDensity1 = []
-      const allDensity2 = []
-      for (i = 0; i < n; i++) {
-          key1 = categories[i] + "_" + year1
-          density_1 = kde( data.map(function(d){  return d[key1]; }) )
-
-          key2 = categories[i] + "_" + year2
-          density_2 = kde( data.map(function(d){  return d[key2]; }) )
-          
-          key = categories[i]
-          allDensity1.push({key: key, density: density_1})
-          allDensity2.push({key: key, density: density_2})
-      }
-    
-      // Add areas
-      svg.selectAll("areas")
-        .data(allDensity1)
-        .join("path")
-		  .attr("class", "igor")
-          .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
-          .datum(function(d){return(d.density)})
-          .attr("fill", "#f5b540")
-          .attr("stroke", "#000")
-          .attr("stroke-width", 0.5)
-          .attr("d",  d3.line()
-              .curve(d3.curveBasis)
-              .x(function(d) { return x(d[0]); })
-              .y(function(d) { return y(d[1]); })
-          )
-
-      svg.selectAll("areas")
-        .data(allDensity2)
-        .join("path")
-		  .attr("class", "igor")
-          .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
-          .datum(function(d){return(d.density)})
-          .attr("fill", "#3e72c7")
-          .attr("stroke", "#000")
-          .attr("stroke-width", 0.5)
-          .attr("d",  d3.line()
-              .curve(d3.curveBasis)
-              .x(function(d) { return x(d[0]); })
-              .y(function(d) { return y(d[1]); })
-          )
-
-      // Y axis label:
-      svg.append("text")
-          .attr("text-anchor", "end")
-          .attr("transform", "rotate(-90)")
-          .attr("y", -margin.left+15)
-          .attr("x", 0 - (height/ 2)+30)
-          .style("fill", "grey")
-          .style("font-size", "18px")
-          .text("Métricas")
-    
-    })
-  }
-  updategraph(1958, 1958);
+        key = categories[i]
+        allDensity1.push({key: key, density: density_1})
+        allDensity2.push({key: key, density: density_2})
+    }
   
+    // Add areas
+    svg.selectAll("areas")
+      .data(allDensity1)
+      .join("path")
+	  .attr("class", "igor")
+        .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
+        .datum(function(d){return(d.density)})
+        .attr("fill", d3.schemeSet3[6])//"#f5b540") 
+        .attr("stroke", "#000")
+        .attr("stroke-width", 0.5)
+        .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+            .x(function(d) { return x(d[0]); })
+            .y(function(d) { return y(d[1]); })
+        )
+
+    svg.selectAll("areas")
+      .data(allDensity2)
+      .join("path")
+	  .attr("class", "igor")
+        .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
+        .datum(function(d){return(d.density)})
+        .attr("fill", d3.schemeSet3[9]) //"#3e72c7")
+        .attr("stroke", "#000")
+        .attr("stroke-width", 0.5)
+        .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+            .x(function(d) { return x(d[0]); })
+            .y(function(d) { return y(d[1]); })
+        )
+
+    // Y axis label:
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left/1.2)
+        .attr("x", 0 - (height/ 2)+30)
+        .style("fill", "grey")
+        .style("font-size", "18px")
+        .text("Métricas")
   
-  // This is what I need to compute kernel density estimation
-  function kernelDensityEstimator(kernel, X) {
-    return function(V) {
-      return X.map(function(x) {
-        return [x, d3.mean(V, function(v) { return kernel(x - v); })];
-      });
-    };
-  }
-  function kernelEpanechnikov(k) {
-    return function(v) {
-      return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-    };
-  }
+  })
+}
+updategraph(1958, 1958);
+
+
+// This is what I need to compute kernel density estimation
+function kernelDensityEstimator(kernel, X) {
+  return function(V) {
+    return X.map(function(x) {
+      return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+    });
+  };
+}
+function kernelEpanechnikov(k) {
+  return function(v) {
+    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+  };
+}
 
--- Data_Vis/assets/js/igor_js2.js
+++ Data_Vis/assets/js/igor_js2.js
@@ -1,22 +1,22 @@
 
-  const years = [
-    1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968,
-    1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979,
-    1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990,
-    1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
-    2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
-    2013, 2014, 2015, 2016, 2017, 2018, 2019
+const years = [
+  1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968,
+  1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979,
+  1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990,
+  1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
+  2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
+  2013, 2014, 2015, 2016, 2017, 2018, 2019
   ]
   
 const dropdownButton1 = d3.select("#dropdown_button_years")
   .attr("id", "button1")
   .style("font-size", "20px")
-  .style("background-color", "#f5b540")
+  .style("background-color", d3.schemeSet3[6])//"#f5b540")
 
 const dropdownButton2 = d3.select("#dropdown_button_years2")
   .attr("id", "button2")
   .style("font-size", "20px")
-  .style("background-color", "#3e72c7")
+  .style("background-color",d3.schemeSet3[9])// "#3e72c7")
 
 dropdownButton1 // Add a button
 .selectAll('myOptions1') // Next 4 lines add 6 options = 6 colors
@@ -48,10 +48,10 @@ dropdownButton1.on("change", function(d) {
 })
 
 dropdownButton2.on("change", function(d) {
-  // recover the option that has been chosen
-  const selectedOption = d3.select(this).property("value")
-
-  // run the updateChart function with this selected option
+  
+  const selectedOption = d3.select(this).property("value") // recover the option that has been chosen
+  
+  //run the updateChart function with this selected option
   //updateChart(selectedOption)
   const otherOption = d3.select("#button1").property("value")
   d3.selectAll(".igor").remove();
